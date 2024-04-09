ac6f05fa3fa47e8f5e92b7502d8df1076734944e
zakrnem <zakrnem@zohomail.com>
2024-04-08 18:50:34 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Where's waldo App: Frontend
+
+This is a photo tagging game where the user has to find a set of characters on a image, the time it takes him to finish it is stored on a leaderboard.
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+
+  return (
+    <>
+      <div className="title">Where's waldo</div>
+    </>
+  )
+}
+
+export default App
