0aefa3e0d75665a1636ebf532716c7e1224347f2
zakrnem <zakrnem@zohomail.com>
2024-04-15 13:54:51 -0600

    Implement absolute coordinates logging for future fetch actions to server

--- src/Gameboard/Gameboard.jsx
+++ src/Gameboard/Gameboard.jsx
@@ -2,19 +2,36 @@ import gameImage from "../assets/waldo-downtown.png";
 import styles from "./Gameboard.module.css";
 import Header from "../Header/Header";
 import SelectionMenu from "../SelectionMenu/SelectionMenu";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Gameboard({ time, menuVisible, setMenuVisible }) {
   const [position, setPosition] = useState([0, 0]);
+  const [coordinates, setCoordinates] = useState([0,0])
+
+  useEffect(() => {
+    console.log("Inner html position: ", position)
+    console.log("Absolute coordinates: ", coordinates)
+  }, [position, coordinates])
+
   const handleClick = (e) => {
-    console.log(e.pageX, e.pageY)
+    const userClickX = e.pageX
+    const userClickY = e.pageY
+
+    const headerHeight = document.querySelector('#header').offsetHeight;
+    const imageWidth = document.querySelector('#gameimage').offsetWidth
+    const imageHeight = document.querySelector('#gameimage').offsetHeight
+
+    const absCoordinateX = Math.round((userClickX/imageWidth)*100)
+    const absCoordinateY = Math.round(( (userClickY - headerHeight ) / imageHeight) * 100)
+    setCoordinates([absCoordinateX, absCoordinateY])
+
     setPosition([e.pageX-38, e.pageY-38]);
     setMenuVisible(true);
   };
   return (
     <div className={styles.gameboard}>
       <Header time={time} />
-      <img src={gameImage} className={styles.gameboard} onClick={handleClick} />
+      <img src={gameImage} id="gameimage" className={styles.gameboard} onClick={handleClick} />
       <SelectionMenu
         menuVisible={menuVisible}
         setMenuVisible={setMenuVisible}
--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -8,7 +8,7 @@ import Logo from "./Logo";
 
 function Header({ time }) {
   return (
-    <div className={styles.header}>
+    <div className={styles.header} id="header">
       <Logo styles={styles} />
       <div className={styles.time}>{time}</div>
 
