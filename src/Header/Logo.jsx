8e71f51d7a5e33694d442d9770373b77a9ebef70
zakrnem <zakrnem@zohomail.com>
2024-05-13 20:52:55 -0600

    Remove gh link in header logo

--- src/Header/Logo.jsx
+++ src/Header/Logo.jsx
@@ -1,9 +1,12 @@
+import { Link, useNavigate } from "react-router-dom";
+
 function Logo({ styles }) {
+  const navigate = useNavigate();
   return (
     <div className={styles.logo}>
-      <a href="https://github.com/zakrnem/" className={styles.title}>
+      <Link to="/" className={styles.title}>
         Where's Wally?
-      </a>
+      </Link>
       <div className={styles.rules}>
         Find all the characters as soon as possible
       </div>
